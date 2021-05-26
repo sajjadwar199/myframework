bbfa96963693c223cda3dea71ed06c7862f73fad
sajjadkareem <sajjadkareem61@gmail.com>
2021-05-26 11:45:03 +0300

    تعديل ملف autoloader

--- app/autoload/autoloader.php
+++ app/autoload/autoloader.php
@@ -1,17 +1,21 @@
 <?php
- function my_autoload($class_name,$pathfolder='app/core/') {
-    if (is_file( $_SERVER['DOCUMENT_ROOT'].'/myframework/'.$pathfolder. $class_name . '.php')) {
-        require_once  $_SERVER['DOCUMENT_ROOT'].'/myframework/'.$pathfolder. $class_name . '.php';
+//استدعاء الملفات تلقائيا
+ //اسم ملف المشروع $folderprojectName
+
+ function my_autoload($class_name,$pathfolder='app/core/',$folderprojectName='myframework') {
+    if (is_file( $_SERVER['DOCUMENT_ROOT'].'/'.$folderprojectName.'/'.$pathfolder. $class_name . '.php')) {
+        require_once  $_SERVER['DOCUMENT_ROOT'].'/'.$folderprojectName.'/'.$pathfolder. $class_name . '.php';
     } else if (is_file('ADMIN/TPL/SOME_FOLDER2/' . $class_name . '.php')) {
         require_once 'ADMIN/TPL/SOME_FOLDER2/' . $class_name . '.php';
     }
 }
 spl_autoload_register("my_autoload");
 
-
-function my_autoload2($class_name,$pathfolder='app/controllers/') {
-    if (is_file( $_SERVER['DOCUMENT_ROOT'].'/myframework/'.$pathfolder. $class_name . '.php')) {
-        require_once  $_SERVER['DOCUMENT_ROOT'].'/myframework/'.$pathfolder. $class_name . '.php';
+//استدعاءكلاسات الموجودة فيcontrollels
+function my_autoload2($class_name,$pathfolder='app/controllers/',$folderprojectName='myframework') {
+     $folderproject='myframework';
+    if (is_file( $_SERVER['DOCUMENT_ROOT'].'/'.$folderprojectName.'/'.$pathfolder. $class_name . '.php')) {
+        require_once  $_SERVER['DOCUMENT_ROOT'].'/'.$folderprojectName.'/'.$pathfolder. $class_name . '.php';
     } else if (is_file('ADMIN/TPL/SOME_FOLDER2/' . $class_name . '.php')) {
         require_once 'ADMIN/TPL/SOME_FOLDER2/' . $class_name . '.php';
     }
